06e1551868f6197ad57e7d81e4bb49eca8c6e0be
johnmuthua <caveomuthama@gmail.com>
2025-09-16 10:54:50 +0300

    fix: add serializer for authentication

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,33 @@
 from django.contrib import admin
-from django.urls import path
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+from django.urls import include, path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-     # Schema generation
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-     # Swagger UI
-    path("api/docs/swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
-    # Redoc UI
-    path("api/docs/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    path("admin/", admin.site.urls),
+    path(
+        "api/v1/",
+        include(
+            [
+                path("auth/", include("authentication.urls")),
+                # Schema generation
+                path("schema/", SpectacularAPIView.as_view(), name="schema"),
+                # Swagger UI
+                path(
+                    "docs/swagger/",
+                    SpectacularSwaggerView.as_view(url_name="schema"),
+                    name="swagger-ui",
+                ),
+                # Redoc UI
+                path(
+                    "docs/redoc/",
+                    SpectacularRedocView.as_view(url_name="schema"),
+                    name="redoc",
+                ),
+            ]
+        ),
+    ),
 ]
