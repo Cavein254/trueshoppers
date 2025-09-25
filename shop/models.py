789110e804d289e1a65e5c9527ae2f1abe4f202a
johnmuthua <caveomuthama@gmail.com>
2025-09-25 12:26:57 +0300

    feat: add shop model

---
+++ shop/models.py
@@ -0,0 +1,35 @@
+from django.conf import settings
+from django.db import models
+from django.utils.text import slugify
+
+
+class Shop(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="shops",
+    )
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True, blank=True, null=True)
+    logo = models.ImageField(upload_to="shops/logos/", blank=True, null=True)
+    description = models.TextField(blank=True)
+    cover_image = models.ImageField(upload_to="shops/covers/", blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(self.name)
+            slug = base_slug
+            counter = 1
+            while Shop.objects.filter(slug=slug).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
+            self.slug = slug
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
